76646d8dc99919acd7f9ce0dc62cab7f8c0ed30e
BRIAN MUTWIRI <103246439+brian-brayoh@users.noreply.github.com>
2023-06-15 16:27:50 +0300

    Add files via upload

---
+++ ringo.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>KidsPlayLearn</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Welcome to KidsPlayLearn</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Mathematics</a></li>
+        <li><a href="#">English</a></li>
+        <li><a href="#">Science</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Resources</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section id="hero">
+      <h2>Fun Learning for Kids</h2>
+      <p>Explore interactive lessons, games, and activities based on the Kenyan curriculum.</p>
+      <a href="#" class="cta-button">Get Started</a>
+    </section>
+    <section id="learning-modules">
+      <h3>Learning Modules</h3>
+      <div class="module">
+        <h4>Mathematics</h4>
+        <p>Engaging math lessons, quizzes, and games to improve numerical skills.</p>
+        <a href="#" class="module-button">Explore</a>
+      </div>
+      <div class="module">
+        <h4>English</h4>
+        <p>Interactive English lessons, vocabulary exercises, and language games.</p>
+        <a href="#" class="module-button">Explore</a>
+      </div>
+      <div class="module">
+        <h4>Science</h4>
+        <p>Fascinating science experiments, educational videos, and hands-on activities.</p>
+        <a href="#" class="module-button">Explore</a>
+      </div>
+    </section>
+    <section id="games-activities">
+      <h3>Games and Activities</h3>
+      <div class="game">
+        <h4>Math Puzzle Challenge</h4>
+        <img src="math-puzzle.jpg" alt="Math Puzzle">
+        <p>Solve exciting math puzzles and test your problem-solving skills.</p>
+        <a href="#" class="game-button">Play Now</a>
+      </div>
+      <div class="game">
+        <h4>Word Explorer</h4>
+        <img src="word-explorer.jpg" alt="Word Explorer">
+        <p>Expand your vocabulary with fun word games and language adventures.</p>
+        <a href="#" class="game-button">Play Now</a>
+      </div>
+      <div class="game">
+        <h4>Memory Master</h4>
+        <img src="memory-master.jpg" alt="Memory Master">
+        <p>Sharpen your memory skills with this exciting memory card game.</p>
+        <a href="#" class="game-button">Play Now</a>
+      </div>
+      <div class="game">
+        <h4>Science Quiz</h4>
+        <img src="science-quiz.jpg" alt="Science Quiz">
+        <p>Test your knowledge of science with this challenging quiz game.</p>
+        <a href="#" class="game-button">Play Now</a>
+      </div>
+    </section>
+    <section id="resources">
+      <h3>Parent and Teacher Resources</h3>
+      <p>Access additional materials, articles, and guides to support learning at home and in the classroom.</p>
+      <a href="#" class="resource-button">Explore Resources</a>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2023 KidsPlayLearn. All rights reserved.</p>
+  </footer>
+</body>
+</html>
